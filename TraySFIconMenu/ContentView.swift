40e51bd4d1c37f96d3f164b313d7de15a9e4ce29
AbdallahKamash <abdallah-kamash@outlook.com>
2026-01-04 07:02:39 +0200

    Initial Commit

---
+++ TraySFIconMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TraySFIconMenu
+//
+//  Created by Abdallah Kamash on 4/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TraySFIconMenu/TraySFIconMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraySFIconMenuApp.swift
+//  TraySFIconMenu
+//
+//  Created by Abdallah Kamash on 4/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct TraySFIconMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
